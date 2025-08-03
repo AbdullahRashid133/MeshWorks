24be83bd9dbc24d498e73c2febae87dd2a579809
Abdullah Rashid <89905186+AbdullahRashid133@users.noreply.github.com>
2025-08-03 14:28:34 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/meshworks/main.go
@@ -0,0 +1,23 @@
+// cmd/meshworks/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"meshworks/internal/meshworks"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := meshworks.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
